3de43757787e180722ac2ef6965e6dba32564596
Mac <mac@Macs-MacBook-Pro.local>
2024-01-24 17:38:41 +0100

    Committing changes before Angular update

---
+++ angular-fron-end/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CustomersComponent } from './customers/customers.component';
+import { AccountsComponent } from './accounts/accounts.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomersComponent,
+    AccountsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
